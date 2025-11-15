eedcf0b45e187ff761f35aae8f1af7c669eeee21
Kobe Thuy <thuynguyentrungdang@gmail.com>
2025-11-15 23:32:16 +0700

    Added missing files to Git

---
+++ BackdoorBanditFika/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using DoorBreach.Fika;
+using DoorBreachFika.Fika;
+using DoorBreachFika.Patches;
+using HarmonyLib;
+
+namespace DoorBreachFika;
+
+public class Plugin
+{
+    public static void Init()
+    {
+        FikaMethods.PluginEnabled();
+        FikaBridge.SendPlantC4PacketEmitted += FikaMethods.SendC4PlantPacket;
+        FikaBridge.SendSyncOpenStatePacketEmitted += FikaMethods.SendSyncOpenStatePacket;
+
+        Harmony harmony = new Harmony("DoorBreach");
+        harmony.PatchAll();
+    }
+}
